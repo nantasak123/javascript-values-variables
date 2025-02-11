2c6db0dcc490c397a31b1d92ae326fb531758852
fluke <chaikhwan32@gmail.com>
2025-02-12 00:05:41 +0700

    Submite JavaScript Values Variables Assignment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let Name = "Karter";
+let surname = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const resterantName = "Chef's Table by Chef James";
+const totalFoodPrice = 15000;
+const activeResterant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalFoodPrice = 5000;
+activeResterant = 2500;
